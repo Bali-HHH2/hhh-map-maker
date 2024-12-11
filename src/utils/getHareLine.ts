d48c40dd9a3a6a4a9b1c71a03ddb8a0dcbfde673
Dash <dbell@btr.pm>
2024-12-11 15:33:22 +0800

    update env

--- src/utils/getHareLine.ts
+++ src/utils/getHareLine.ts
@@ -1,6 +1,6 @@
 const getHareLine = async () => {
   const request = await fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/115xv4g_5lk8u_tPBQqj_f8QnE59oQQwCU2Rbcd_2aik/values/${new Date().getFullYear()}?alt=json&key=AIzaSyCJBl1U-PuLxlX6CUuMVjeRMczZDoE3eNU`
+    `https://sheets.googleapis.com/v4/spreadsheets/115xv4g_5lk8u_tPBQqj_f8QnE59oQQwCU2Rbcd_2aik/values/${new Date().getFullYear()}?alt=json&key=${import.meta.env.VITE_GOOGLEMAPS_API_KEY}`
   )
   const response = await request.json()
   return response.values
