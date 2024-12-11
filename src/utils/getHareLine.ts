464690c7aa36e4fa6546aed92232748e9b6c5b56
Dash <dbell@btr.pm>
2024-12-11 15:27:44 +0800

    update key

--- src/utils/getHareLine.ts
+++ src/utils/getHareLine.ts
@@ -1,6 +1,6 @@
 const getHareLine = async () => {
   const request = await fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/115xv4g_5lk8u_tPBQqj_f8QnE59oQQwCU2Rbcd_2aik/values/${new Date().getFullYear()}?alt=json&key=AIzaSyCqwiqxassrTcxREEM76PUP24qQ5GQFUm0`
+    `https://sheets.googleapis.com/v4/spreadsheets/115xv4g_5lk8u_tPBQqj_f8QnE59oQQwCU2Rbcd_2aik/values/${new Date().getFullYear()}?alt=json&key=AIzaSyCJBl1U-PuLxlX6CUuMVjeRMczZDoE3eNU`
   )
   const response = await request.json()
   return response.values
